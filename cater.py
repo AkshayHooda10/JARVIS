de57a830482f93cb0b40e9262bac96be83320015
Akshay Hooda <akshay21csu312@ncuindia.edu>
2024-01-19 23:16:51 +0530

    Fixed mouse click bug! added user input

--- cater.py
+++ cater.py
@@ -8,7 +8,7 @@ drawing_utils = mp.solutions.drawing_utils
 screen_width , screen_height = pyautogui.size()
 cap = cv2.VideoCapture(0)
 index_y = 0
-max_duration = 10
+max_duration = 0
 user_response = "y"
 
 while user_response == "y":
@@ -28,25 +28,25 @@ while user_response == "y":
                     x = int(landmark.x*frame_width)
                     y = int(landmark.y*frame_height)
                     
-                if id == 8:
-                    cv2.circle(img=frame , center = (x,y) , radius = 10 , color = (0,255,255))
-                    index_x = screen_width/frame_width*x
-                    index_y = screen_height/frame_height*y
-                    pyautogui.moveTo(index_x, index_y)
-                if id == 4:
-                    cv2.circle(img=frame , center = (x,y) , radius = 10 , color = (0,255,255))
-                    thumb_x = screen_width/frame_width*x
-                    thumb_y = screen_height/frame_height*y
-                    print('outside' , abs(index_y - thumb_y))
-                    if abs(index_y - thumb_y) < 20: 
-                        pyautogui.click()
-                        pyautogui.sleep(1)
-    
+                    if id == 8:
+                        cv2.circle(img=frame , center = (x,y) , radius = 10 , color = (0,255,255))
+                        index_x = screen_width/frame_width*x
+                        index_y = screen_height/frame_height*y
+                        pyautogui.moveTo(index_x, index_y)
+                    if id == 4:
+                        cv2.circle(img=frame , center = (x,y) , radius = 10 , color = (0,255,255))
+                        thumb_x = screen_width/frame_width*x
+                        thumb_y = screen_height/frame_height*y
+                        print('outside' , abs(index_y - thumb_y))
+                        if abs(index_y - thumb_y) < 20: 
+                            pyautogui.click()
+                            pyautogui.sleep(1)
+            
 
         elapsed_time = time.time()- start_time
         print('Elapsed time = ' , round(elapsed_time , 2), "s")
         cv2.imshow('C.A.T.E.R' , frame)
         cv2.waitKey(1)
-    user_response = input("Do you want to continue? y/n")
+
 cap.release()
 cv2.destroyAllWindows()
