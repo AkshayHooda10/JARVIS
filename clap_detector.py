c384d749ab8f479b12f61c5045a0577ec588da98
Akshay Hooda <112803446+AkshayHooda10@users.noreply.github.com>
2024-01-22 23:42:37 +0530

    Update clap_detector.py HAS BUGS

--- clap_detector.py
+++ clap_detector.py
@@ -1,26 +1,29 @@
 import sounddevice as sd 
 import numpy as np
 
-threshold = 15
+threshold = 20
 clap = False 
 
-def detect_clap(indata , frames , time , status):
-    global clap 
-    volume_norm = np.linalg.norm(indata)*10
-    if volume_norm>threshold:
-        print("Clapped!")
-        clap = True
-    pass
-
-def listen_for_clap():
-    with sd.InputStream(callback=detect_clap):
-        return sd.sleep(1000)
-
-if __name__ == "__main__":
-    while True:
-        listen_for_clap()
-        if clap == True:
-            break
-        else:
+while user_input == y:
+        def detect_clap(indata , frames , time , status):
+            global clap 
+            volume_norm = np.linalg.norm(indata)*10
+            if volume_norm>threshold:
+                print("Clapped!")
+                clap = True
+                user_input = input("Do you want to run this again? : y/n")
             pass
 
+        def listen_for_clap():
+            with sd.InputStream(callback=detect_clap):
+                return sd.sleep(1000)
+            
+        
+        if __name__ == "__main__":
+            while True:
+                listen_for_clap()
+                if clap == True:
+                    break
+                else:
+                   pass
+        //asking for input from user !! HAS BUGS !!
