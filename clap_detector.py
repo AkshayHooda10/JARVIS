a3b8e5fc338a82d8dde15646044673ed0e6763d9
Akshay Hooda <112803446+AkshayHooda10@users.noreply.github.com>
2024-01-22 23:52:13 +0530

    Update clap_detector.py !! Fixed clap threshold

--- clap_detector.py
+++ clap_detector.py
@@ -3,27 +3,25 @@ import numpy as np
 
 threshold = 20
 clap = False 
+user_input = 'y'
 
-while user_input == y:
         def detect_clap(indata , frames , time , status):
-            global clap 
+            global clap, user_input 
             volume_norm = np.linalg.norm(indata)*10
             if volume_norm>threshold:
                 print("Clapped!")
                 clap = True
-                user_input = input("Do you want to run this again? : y/n")
-            pass
+                user_input = input("Do you want to continue this program : y/n ?")
 
         def listen_for_clap():
             with sd.InputStream(callback=detect_clap):
                 return sd.sleep(1000)
-            
-        
+                
         if __name__ == "__main__":
-            while True:
+            while user_input.lower()=='y':
+                clap = False
                 listen_for_clap()
                 if clap == True:
                     break
-                else:
-                   pass
-        //asking for input from user !! HAS BUGS !!
+            
+        
